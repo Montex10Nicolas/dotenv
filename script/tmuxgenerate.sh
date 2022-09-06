c3edda6b97b3bd17d45ec7cad7bdfcb745edb46b
montex <montexdev@protonmail.com>
2022-09-07 00:58:48 +0200

    moved meslo and added tmux plugin downloads

---
+++ script/tmuxgenerate.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/wfxr/tmux-power .tmux/plugins/tmux-power
+
+git clone https://github.com/wfxr/tmux-net-speed .tmux/plugins/tmux-net-speed
+
+git clone https://github.com/tmux-plugins/tmux-prefix-highlight .tmux/plugins/highlight
+
+cp -r .tmux ~/.tmux
