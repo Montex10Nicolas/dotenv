24a7d9cee1d24b47d10f1c08420a0d14a9099afa
Nicolas Montex <montedev@protonmail.com>
2022-10-03 19:12:41 +0200

    removed

--- script/ssh-key.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install xclip 
-
-ssh-keygen -t ed25519 -C "montexdev@protonmail.com"
-
-eval "$(ssh-agent -s)"
-
-ssh-add ~/.ssh/id_ed25519
-
-cat ~/.ssh/id_ed25519.pub|xclip -i -selection clipboard
