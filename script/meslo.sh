f2ec77cf35d74bb8c496e401366730cd050cb687
Nicolas Montex <montexdev@protonmail.com>
2022-10-08 00:38:34 +0200

    meslo downlaod

--- script/meslo.sh
+++ script/meslo.sh
@@ -1,4 +1,4 @@
-#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
 sudo apt install font-manager
 mkdir temp
 mv ./Meslo.zip ./temp/
