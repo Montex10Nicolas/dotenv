9b5ea4efa86f37d22ff0bbc6c764f26bd6468997
Nicolas Montecchiani <montexdev@protonmail.com>
2024-04-23 22:46:46 +0200

    Winget install

---
+++ winget.ps1
@@ -0,0 +1,9 @@
+﻿foreach($line in Get-Content .\winget_install.txt) {
+    if ($line[0] -match "#" -or $line.Length -lt 1){
+        continue
+    }
+
+    winget install $line;
+}
+
+Write-Host "Execution completed";
